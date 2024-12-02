f569ba9a27805cb4ea6d669c791b845f2acf5195
S4M8 <matt.urrea.code@gmail.com>
2024-12-01 23:48:47 -0700

    sort and compare array

--- day_1/main.zig
+++ day_1/main.zig
@@ -1,23 +1,23 @@
-// importing the standard library
 const std = @import("std");
 
-// Created a basic list of unsigned 8bit integers.
-// Thought here being that I will not need to allocate all but the smallest unit for these arrays that will not change.
-const left_list = [_]u8{ 3, 4, 2, 1, 3, 3 };
-const right_list = [_]u8{ 4, 3, 5, 3, 9, 3 };
-
-// Pair up the smallest number in the left list with the smallest number in the right list
-// sort int[] from smallest to largest
-// find the difference between each
-
 pub fn main() !void {
-    // better iterating over array behavior.
-    var i: u8 = 0;
-    while (i < 5) : (i += 1) {
-        const left = left_list[i];
-        const right = right_list[i];
-        std.debug.print("#{}: {},{}\n", .{ i, left, right });
-        const dex = left - right;
-        std.debug.print("Difference: {}", .{dex});
+    const left_list = [_]u8{ 3, 4, 2, 1, 3, 3 };
+    const right_list = [_]u8{ 4, 3, 5, 3, 9, 3 };
+
+    // memory allocator to create sortable slices
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = general_purpose_allocator.allocator();
+
+    const left_sorted = try gpa.dupe(u8, &left_list);
+    const right_sorted = try gpa.dupe(u8, &right_list);
+
+    std.mem.sort(u8, left_sorted, {}, comptime std.sort.asc(u8));
+    std.mem.sort(u8, right_sorted, {}, comptime std.sort.asc(u8));
+
+    // Slice Iteration allows fo simultaneous iteration
+    for (left_sorted, right_sorted, 0..) |left, right, index| {
+        // conditional to handle potential underflow
+        const diff = if (left > right) left - right else right - left;
+        std.debug.print("Index {}: Left {}, Right {}, Difference {}\n", .{ index, left, right, diff });
     }
 }
